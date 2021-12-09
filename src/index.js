8172c59c5ae10e32583a67af4c3b17b23a2547e2
Azonkeu <mazonkeu@gmail.com>
2021-12-09 04:00:24 +0100

    Edit the html file

--- src/index.js
+++ src/index.js
@@ -3,7 +3,8 @@ import './style.css';
 
 function component() {
     const element = document.createElement('div');
-  
+
+
     // Lodash, now imported by this script
     element.innerHTML = _.join(['Hello', 'webpack'], ' ');
     element.classList.add('hello');
@@ -12,3 +13,28 @@ function component() {
   }
   
   document.body.appendChild(component());
+
+
+
+
+  const addArr = [
+    {description: 'clean apartment'},
+    {completed: true},
+    {index: 0}
+  ];
+
+  document.getElementById('save').addEventListener('click', () => {
+    let  ordering = document.getElementById('order');
+    let data = document.getElementById('textInput').value;
+    addArr.push(data);
+    let item = document.createElement(data);
+    let newItem = document.createElement('li');
+    ordering.appendChild(newItem);
+    newItem.appendChild(item); 
+  });
+  console.log(addArr);
+
+  
+
+  
+
