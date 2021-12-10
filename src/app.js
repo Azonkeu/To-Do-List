17b42e8b0186efc9acda5420cb6048335c3af958
Azonkeu <mazonkeu@gmail.com>
2021-12-10 17:04:07 +0100

    Add the javascript code to add and remove tasks

--- src/app.js
+++ src/app.js
@@ -1,83 +1,146 @@
-const todoArr = [
-  {
-    description: 'Wash the dishes',
-    completed: false,
-    index: 0,
-  },
-  {
-    description: 'Complete To Do list project',
-    completed: false,
-    index: 1,
-  },
-  {
-    description: 'Manage all your lists in one place',
-    completed: false,
-    index: 2,
-  },
-];
+const todoArr = [];
+/* eslint-disable no-use-before-define */
+const add = () => {
+  const list = document.querySelector('.task-list');
+  const itemsLocal = JSON.parse(localStorage.getItem('itemsLocal'));
+  todoArr.splice(0, todoArr.length, ...itemsLocal);
+  list.innerHTML = '';
+  for (let i = 0; i < todoArr.length; i += 1) {
+    const { description } = itemsLocal[i];
+    const listItem = document.createElement('li');
+    listItem.className = 'list-item';
+    listItem.innerHTML = `<input class="list-input" type="checkbox"><textarea name="textarea cols="30" class="item-details">${description}</textarea><i class="fas fa-trash-alt"></i> <i class="fas fa-ellipsis-v"></i>`;
+    list.appendChild(listItem);
+  }
+  const listInput = document.querySelectorAll('.list-input');
+  interact(listInput);
+  clearList(listInput);
+  removeItem();
+  updateValues();
+};
 
-// add to local storage
+const form = document.getElementById('form-id');
+const showTasks = () => {
+  form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const task = form.elements[0].value;
 
-export function storeList() {
-  localStorage.setItem('todoArr', JSON.stringify(todoArr));
-}
-
-const list = document.querySelector('.ullist');
-// add items to dom
-export function addList(array, element) {
-  array.forEach((item) => {
-    const listItems = document.createElement('li');
-    listItems.classList.add('list-item');
-    element.appendChild(listItems);
-    listItems.innerHTML = `
-      <input class="list-input" type="checkbox" id="${item.index} ">
-      <label for="${item.index}">${item.description}
-      <i class="fas fa-ellipsis-v"></i>
-       `;
+    const data = {
+      description: task,
+      completed: false,
+      index: todoArr.length,
+    };
+    form.reset();
+    todoArr.push(data);
+    localStorage.setItem('itemsLocal', JSON.stringify(todoArr));
+    add();
   });
-}
-addList(todoArr, list);
+};
 
-// function
-export function checked() {
-  const checkbox = document.querySelectorAll('.list-input');
-  checkbox.forEach((item) => {
+const removeItem = () => {
+  const button = document.querySelectorAll('.fa-trash-alt');
+  button.forEach((item) => {
+    const parent = item.parentNode;
+    const superParent = parent.parentNode;
+    const index = Array.prototype.indexOf.call(superParent.children, parent);
+    const listInput = parent.firstChild;
+    item.addEventListener('click', () => {
+      const itemsLocal = JSON.parse(localStorage.getItem('itemsLocal'));
+      todoArr.splice(0, todoArr.length, ...itemsLocal);
+      if (listInput.hasAttribute('checked')) {
+        parent.remove();
+        todoArr.splice(index, 1);
+      }
+      for (let i = 0; i < todoArr.length; i += 1) {
+        todoArr[i].index = i ;
+      }
+      localStorage.setItem('itemsLocal', JSON.stringify(todoArr));
+      add();
+    });
+  });
+};
+
+const clearList = () => {
+  const getClearElement = document.querySelector('.clear-task');
+  getClearElement.addEventListener('click', () => {
+    for (let i = 0; i < todoArr.length; i += 1) {
+      if (todoArr[i].completed) {
+        todoArr.splice(i, 1);
+      }
+    }
+    for (let i = 0; i < todoArr.length; i += 1) {
+        todoArr[i].index = i + 1;
+    }
+    localStorage.setItem('itemsLocal', JSON.stringify(todoArr));
+    add();
+  });
+};
+
+const updateValues = () => {
+  const itemDetails = document.querySelectorAll('item-details');
+  itemDetails.forEach((item) => {
+    const parent = item.parentNode;
+    const superParent = parent.parentNode;
+    const index = Array.prototype.indexOf.call(superParent.children, parent);
     item.addEventListener('change', () => {
-      const parent = item.parentNode;
-      const superParent = parent.parentNode;
-      const index = Array.prototype.indexOf.call(superParent.children, parent);
-      const parentItem = todoArr[index].completed;
-      if (parentItem) {
-        todoArr[index].completed = false;
-        parent.classList.remove('checked');
-        localStorage.setItem('todoArr', JSON.stringify(todoArr));
-      } else {
-        todoArr[index].completed = true;
-        parent.classList.add('checked');
-        localStorage.setItem('todoArr', JSON.stringify(todoArr));
-      }
-      storeList();
+        todoArr[index].description = item.value;
+      localStorage.setItem('itemsLocal', JSON.stringify(todoArr));
     });
   });
-}
-checked();
+};
 
-export function showStorage() {
+const textDecor = (listInput) => {
+  listInput.forEach((item) => {
+    if (item.hasAttribute('checked')) {
+      item.nextSibling.style.textDecoration = 'line-through';
+    } else {
+      item.nextSibling.style.textDecoration = 'none';
+    }
+  });
+};
+
+export default function storageForTask() {
   window.addEventListener('load', () => {
-    const localData = JSON.parse(localStorage.getItem('todoArr'));
-    todoArr.splice(0, todoArr.length, ...localData);
-    const box = document.querySelectorAll('.list-input');
-
-    box.forEach((item) => {
+    add();
+    const listInput = document.querySelectorAll('.list-input');
+    const itemsLocal = JSON.parse(localStorage.getItem('itemsLocal'));
+    listInput.forEach((item) => {
       const parent = item.parentNode;
       const superParent = parent.parentNode;
       const index = Array.prototype.indexOf.call(superParent.children, parent);
-      const parentItem = todoArr[index].completed;
-
-      if (parentItem === true) {
+      const currentItem = itemsLocal[index].completed;
+      if (currentItem) {
         item.setAttribute('checked', '');
+        parent.lastChild.style.display = 'block';
       }
     });
+    textDecor(listInput);
   });
-}
-showStorage();
+};
+
+const interact = (listInput) => {
+  listInput.forEach((item) => {
+    item.addEventListener('change', () => {
+      const itemsLocal = JSON.parse(localStorage.getItem('itemsLocal'));
+      const parent = item.parentNode;
+      const superParent = parent.parentNode;
+      const index = Array.prototype.indexOf.call(superParent.children, parent);
+      const currentItem = itemsLocal[index].completed;
+      if (currentItem) {
+        item.removeAttribute('checked');
+        parent.lastChild.style.display = 'none';
+        itemsLocal[index].completed = false;
+      } else {
+        item.setAttribute('checked', '');
+        parent.lastChild.style.display = 'block';
+        itemsLocal[index].completed = true;
+      }
+      textDecor(listInput);
+      localStorage.setItem('itemsLocal', JSON.stringify(itemsLocal));
+      todoArr.splice(0, todoArr.length, ...itemsLocal);
+    });
+  });
+};
+
+showTasks();
+
--- src/index.js
+++ src/index.js
@@ -1,9 +1,4 @@
 import './style.css';
-import {
-  showStorage, checked, storeList, addList,
-} from './app.js';
-/* eslint-disable no-unused-expressions */
-addList;
-storeList;
-checked;
-showStorage;
+import storageForTask from './app.js';
+
+storageForTask();
