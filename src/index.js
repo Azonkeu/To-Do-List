3d734a91173b24c731390042fbae4a60a03146a2
Azonkeu <mazonkeu@gmail.com>
2021-12-10 08:18:57 +0100

    Fix linters issues

--- src/index.js
+++ src/index.js
@@ -2,9 +2,8 @@ import './style.css';
 import {
   showStorage, checked, storeList, addList,
 } from './app.js';
-
+/* eslint-disable no-unused-expressions */
 addList;
 storeList;
 checked;
 showStorage;
-
